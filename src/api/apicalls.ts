2c2c703678e7457a06ab033a7648a2ab39d42b39
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2024-01-25 19:04:47 +0500

    added api routes

---
+++ src/api/apicalls.ts
@@ -0,0 +1,29 @@
+const apiKey: string = "5a7531a791704bb4b24befa4738dd37c";
+
+export const imageBaseUrl= (size:string,path :string) => `https://image.tmdb.org/t/p/${size}${path}`
+
+export const nowPlayingMovies: string = `
+
+https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey};
+`
+export const upcomingMovies: string = `
+https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey};
+
+`
+export const popularMovies: string = `
+https://api.themoviedb.org/3/movie/popular?api_key=${apiKey};
+`
+
+export const searchMovies = (keyword: string) => `
+https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${keyword};
+`
+
+export const movieDetails = (movieId: string) => `
+https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey};
+`
+
+export const movieCastDetails = (movieId: string) => `
+https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey};
+
+
+`
